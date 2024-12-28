ceeb0f4a9c384da5dfc35aec5878d72720d38541
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2024-12-28 11:52:45 +0200

    Update ISerializer.java

--- JCook/src/ISerializer.java
+++ JCook/src/ISerializer.java
@@ -1,6 +1,9 @@
 import java.util.List;
 
 public interface ISerializer {
+	public boolean recipeExistsByName(String name);
+	public boolean recipeExistsById(int id);
 	public void saveRecipe(Recipe recipe);
-	public List<Recipe> getData();
+	public Recipe getRecipeByName(String name);
+	public Recipe getRecipeById(int id);
 }
