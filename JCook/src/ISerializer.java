0663ef24a728d1c5432922078ecd698a962e664e
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2025-01-01 14:10:06 +0200

    refactor: added comments to all of the functions in ISerializer

--- JCook/src/ISerializer.java
+++ JCook/src/ISerializer.java
@@ -1,10 +1,38 @@
 import java.util.List;
 
 public interface ISerializer {
+	// Checks if a recipe exists in the DB by its name
 	public boolean recipeExistsByName(String name);
+	
+	// Checks if a recipe exists in the DB by its id
 	public boolean recipeExistsById(int id);
-	public void saveRecipe(Recipe recipe);
+	
+	// Save a recipe to the DB. Do nothing if name already exists.
+	// Return true if added successfully and false if not added
+	public boolean saveRecipe(Recipe recipe);
+	
+	// Delete a recipe from the DB. Return true if deleted successfully 
+	// and false if not (if name doesn't exist, do nothing and return false)
+	public boolean deleteRecipeByName(String name);
+	
+	// Delete a recipe from the DB. Return true if deleted successfully 
+	// and false if not (if id doesn't exist, do nothing and return false)
+	public boolean deleteRecipeById(int id);
+	
+	// Retrieve a recipe from the DB. Return null if not found
 	public Recipe getRecipeByName(String name);
+	
+	// Retrieve a recipe from the DB. Return null if not found
 	public Recipe getRecipeById(int id);
+	
+	// Return in a list all of the ids of the recipes currently stored 
+	// in the DB (Return an empty list if the DB doesn't contain any recipe)
 	public List<Integer> getRecipeIds();
+	
+	// Return in a list all of the names of the recipes currently stored 
+	// in the DB (Return an empty list if the DB doesn't contain any recipe)
+	public List<String> getRecipeNames();
+	
+	// Retrieve the names of all of the recipes in the DB of a specific category
+	public List<Integer> getRecipeNamesByCategory(String category);
 }
